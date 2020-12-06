65dfc1685af714d4cd248f14c8caaa783f153992
Vasiliki Manoli <v.manoli@kaizengaming.com>
2020-12-06 15:12:33 +0200

    Day1 solution

--- src/Days/Day1.cs
+++ src/Days/Day1.cs
@@ -9,12 +9,45 @@ namespace AdventOfCode
         {
             int answer = 0;
 
+            string[] lines = text.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = lines.Select(l => Int32.Parse(l)).ToArray();
+
+            foreach (int number1 in numbers)
+            {
+                foreach (var number2 in numbers)
+                {
+                    if (number1 + number2 == 2020)
+                    {
+                        answer = number1 * number2;
+                        break;
+                    }
+                }
+            }
+
             Console.WriteLine($"Day1:PartOne: {answer}");
         }
 
         public void PlayPartTwo(string text)
         {
             int answer = 0;
+
+            string[] lines = text.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            int[] numbers = lines.Select(l => Int32.Parse(l)).ToArray();
+
+            foreach (int number1 in numbers)
+            {
+                foreach (var number2 in numbers)
+                {
+                    foreach (var number3 in numbers)
+                    {
+                        if (number1 + number2 + number3 == 2020)
+                        {
+                            answer = number1 * number2 * number3;
+                            break;
+                        }
+                    }
+                }
+            }
 
             Console.WriteLine($"Day1:PartOne: {answer}");
         }
